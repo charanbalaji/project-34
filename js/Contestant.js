f0dab08024843c8e201d237f7130ef698747a95a
charanbalaji <88437189+charanbalaji@users.noreply.github.com>
2021-11-09 12:45:39 +0530

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,36 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+    contestantCountRef.on("value",(data)=>{
+      contestantCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      contestantCount: count
+    });
+  }
+
+   update(){
+    var contestantIndex = "contestants/contestant" + this.index;
+    database.ref(contestantIndex).set({
+      name:10,
+      answer:10
+    });
+  } 
+
+  static getPlayerInfo(){
+    var contestantInfoRef = database.ref('contestants');
+    contestantInfoRef.on("value",(data)=>{
+      allContestants = data.val();
+    })
+  }
+ 
+}
